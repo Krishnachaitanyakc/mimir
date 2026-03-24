112605e8e254ff1dc3fb9881f02dd782b9cea5eb
Charles Korn <charles.korn@grafana.com>
2026-03-24 20:23:15 +1100

    Remove old stats type from selectors

--- pkg/streamingpromql/optimize/plan/commonsubexpressionelimination/range_vector_operator_test.go
+++ pkg/streamingpromql/optimize/plan/commonsubexpressionelimination/range_vector_operator_test.go
@@ -1081,7 +1081,6 @@ func TestRangeVectorOperator_Stats(t *testing.T) {
 		labels.MustNewMatcher(labels.MatchEqual, "env", "test"),
 	}
 
-	stats := types.NewQueryStats()
 	timeRange := types.NewInstantQueryTimeRange(timestamp.Time(0))
 	selector := selectors.NewRangeVectorSelector(
 		&selectors.Selector{
@@ -1093,7 +1092,6 @@ func TestRangeVectorOperator_Stats(t *testing.T) {
 			Subsets:                  []selectors.Subset{{Filter: subset}},
 		},
 		memoryConsumptionTracker,
-		stats,
 	)
 
 	buffer := NewRangeVectorDuplicationBuffer(selector, memoryConsumptionTracker, timeRange, log.NewNopLogger())
